02b79285b95dbedf7b5bef7123954245a4788dfb
Rajendra Acharya <acharyarajendra48@gmail.com>
2026-07-02 15:50:56 +0530

    ready to deploye

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:3000",
+  baseURL: "https://taskflow-64aa.onrender.com",
 });
 
 export const getTasks = () => api.get("/tasks");
